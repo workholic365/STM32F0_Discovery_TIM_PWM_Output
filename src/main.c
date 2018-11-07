7939c9ab60a22191ab672812162a5999b3403c48
root <root@jutze-VirtualBox>
2018-11-07 10:25:46 +0800

    add "test for TrueSTUDIO integrated with git" in main

--- src/main.c
+++ src/main.c
@@ -56,7 +56,7 @@ void TIM_Config(void);
   */
 int main(void)
 {
-	//test for TrueSTUDIO integrated with git
+	//test for TrueSTUDIO integrated with
   /*!< At this stage the microcontroller clock setting is already configured, 
        this is done through SystemInit() function which is called from startup
        file (startup_stm32f0xx.s) before to branch to application main.
